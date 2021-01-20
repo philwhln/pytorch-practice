7ac1715ef2b6a1b1614692eede090156ceb98252
Phil Whelan <phil123@gmail.com>
2021-01-20 12:17:23 -0800

    simple nn.Linear

--- simple-nn-linear.py
+++ simple-nn-linear.py
@@ -1,10 +1,6 @@
 import torch
-from torch import Tensor, tensor, optim
+from torch import Tensor, tensor, optim, nn
 import matplotlib.pyplot as plt
-
-
-def model(x: Tensor, w1: Tensor, w2: Tensor, b: Tensor) -> Tensor:
-    return (w1 * (x ** 2)) + (w2 * x) + b
 
 
 def loss_fn(y_hat: Tensor, y: Tensor) -> Tensor:
@@ -12,9 +8,9 @@ def loss_fn(y_hat: Tensor, y: Tensor) -> Tensor:
     return squared_diffs.mean()
 
 
-def calculate_loss(x, y, params, is_train):
+def calculate_loss(model, x, y, is_train):
     with torch.set_grad_enabled(is_train):
-        y_hat = model(x, *params)
+        y_hat = model(x)
         loss = loss_fn(y_hat, y)
     assert loss.requires_grad == is_train
     return loss
@@ -24,8 +20,8 @@ def main():
 
     learning_rate = 1e-2
 
-    x = tensor([35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4])
-    y = tensor([0.5, 14.0, 15.0, 28.0, 11.0, 8.0, 3.0, -4.0, 6.0, 13.0, 21.0])
+    x = tensor([35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4]).unsqueeze(1)
+    y = tensor([0.5, 14.0, 15.0, 28.0, 11.0, 8.0, 3.0, -4.0, 6.0, 13.0, 21.0]).unsqueeze(1)
 
     num_examples = x.shape[0]
     shuffled_indices = torch.randperm(num_examples)
@@ -37,25 +33,24 @@ def main():
     x_val = x[val_indices]
     y_val = y[val_indices]
 
-    params = torch.rand(3, requires_grad=True)
-    optimizer = optim.Adam(params=[params], lr=learning_rate)
+    model = nn.Linear(in_features=1, out_features=1, bias=True)
+    optimizer = optim.Adam(params=model.parameters(), lr=learning_rate)
 
-    for epoch in range(10000):
+    for epoch in range(5000):
 
-        train_loss = calculate_loss(x_train, y_train, params, is_train=True)
+        train_loss = calculate_loss(model, x_train, y_train, is_train=True)
 
         optimizer.zero_grad()
         train_loss.backward()
         optimizer.step()
 
-        val_loss = calculate_loss(x_val, y_val, params, is_train=False)
+        val_loss = calculate_loss(model, x_val, y_val, is_train=False)
         if epoch % 500 == 0:
-            print(f'epoch = {epoch}  params = {params}  params.grad = {params.grad}  ' +
-                  f'train loss = {train_loss}  val loss = {val_loss}')
+            print(f'epoch = {epoch}  train loss = {train_loss}  val loss = {val_loss}')
 
     with torch.no_grad():
-        line_x = torch.linspace(min(x), max(x_train), 100)
-        line_y = model(line_x, *params)
+        line_x = torch.linspace(min(x.squeeze(1)), max(x_train.squeeze(1)), 100).unsqueeze(1)
+        line_y = model(line_x)
         plt.scatter(x_train, y_train)
         plt.scatter(x_val, y_val)
         plt.plot(line_x, line_y)
