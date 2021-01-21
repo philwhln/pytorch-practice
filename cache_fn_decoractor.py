87bd38781ffa37aa43cb0f0073e829c6899531a6
Phil Whelan <phil123@gmail.com>
2021-01-21 11:38:12 -0800

    add pickle caching for prepare_data

---
+++ cache_fn_decoractor.py
@@ -0,0 +1,25 @@
+import os
+import pickle
+from pathlib import Path
+
+CACHE_DIR = Path(__file__).parent / 'cache'
+
+
+def cache(cache_id):
+    path = CACHE_DIR / cache_id
+    if not CACHE_DIR.exists():
+        CACHE_DIR.mkdir()
+
+    def decorator(fn):
+        def wrapped(*args, **kwargs):
+            if os.path.exists(path):
+                with open(path, 'rb') as f:
+                    return pickle.load(f)
+            res = fn(*args, **kwargs)
+            with open(path, 'wb') as f:
+                pickle.dump(res, f)
+            return res
+
+        return wrapped
+
+    return decorator
